0eeba8d2ebdb7bc8e3b0b16c2707b69ce319f30b
Daviczrn07 <davi.cezarino@edu.senai.br>
2026-09-10 15:06:34 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    internal class Pessoa
+    {
+        private string _nome;
+
+        public string Nome
+        {
+            get { return _nome; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Nome não pode ser nulo");
+                }
+                else
+                {
+                    _nome = value;
+                }
+            }
+        }
+    }
+}
