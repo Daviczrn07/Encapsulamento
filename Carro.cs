377431105d41904380be513337a5024528ba8700
Daviczrn07 <davi.cezarino@edu.senai.br>
2026-09-10 15:36:06 -0300

    Exer3

---
+++ Carro.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Encapsulamento
+{
+    internal class Carro
+    {
+        private int _velocidadeAtual;
+
+        public int VelocidadeAtual
+        {
+            get { return _velocidadeAtual; }
+        }
+        public void Acelerar()
+        {
+            _velocidadeAtual += 10;
+        }
+
+        public void Frear()
+        {
+            if (_velocidadeAtual >= 10)
+            {
+                _velocidadeAtual -= 10;
+            }
+            else
+            {
+                _velocidadeAtual = 0;
+            }
+        }
+    }
+}
